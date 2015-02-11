914c92921e5048d3e6c0be92da008b7ff3177c97
levijohnston <johns464@mail.chapman.edu>
2015-02-11 00:34:37 -0800

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "grid.h"
+#include "game.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <unistd.h>
+
+int main()
+{
+	game Life;
+	Life.runGame();
+
+	return 0;
+};
